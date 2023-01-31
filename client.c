8a454ef20ad0cc537a00439ce5b9438e0e1fbc87
Kate Sychenko <busy.sychenko@gmail.com>
2023-01-31 19:13:23 +0400

    server and client communication via SAYS, add client address & port indentification

--- client.c
+++ client.c
@@ -1,3 +1,4 @@
+// by duckate, Kate Sychenko
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,8 +6,9 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+#define SIZE 1024
 #define PORT 8080
-
+#define SERVER_ADDRESS "127.0.0.1"
 
 int main(int argc, char const* argv[]) {  
     
@@ -15,10 +17,10 @@ int main(int argc, char const* argv[]) {
     char buffer[1024] = { 0 };
     char* hello = "Hello from client\n";
 
-// create a socket
-// -1 if error
+    // create a socket
+    // -1 if error
     if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("CLIENT: socket creation failed");
+        perror("[-] CLIENT: socket creation failed");
         exit(EXIT_FAILURE);
     }
 
@@ -26,30 +28,20 @@ int main(int argc, char const* argv[]) {
     serv_addr.sin_port = htons(PORT);
     serv_addr.sin_addr.s_addr = INADDR_ANY;
 
-//  if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)
-//         <= 0) {
-//         perror(
-//             "\nInvalid address/ Address not supported \n");
-//         exit(EXIT_FAILURE);
-//     }
-
+    if (inet_pton(AF_INET, SERVER_ADDRESS, &serv_addr.sin_addr) <= 0) {
+            perror("[-] LIENT: Invalid address/ Address not supported");
+            exit(EXIT_FAILURE);
+        }
     
-    if ((status
-         = connect(client_fd, (struct sockaddr*)&serv_addr,
-                   sizeof(serv_addr)))
-        < 0) {
-        perror("CLIENT: Connection Failed\n");
+    if ((status = connect(client_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)))< 0) {
+        perror("[-] CLIENT: Connection Failed\n");
         exit(EXIT_FAILURE);
     }
 
     send(client_fd, hello, strlen(hello), 0);
-    printf("CLIENT: Hello message sent\n");
-    valread = read(client_fd, buffer, 1024);
-    printf("CLIENT BUFFER: %s\n", buffer);
- 
-printf("client_fd value    = %d\n", client_fd);
-
-    read(STDIN_FILENO, buffer, 10);
+    printf("[+] CLIENT: Hello message sent\n");
+    valread = read(client_fd, buffer, SIZE);
+    printf("[+] SERVER SAYS: %s\n", buffer);
 
     // closing the connected socket
     close(client_fd);
--- server.c
+++ server.c
@@ -1,3 +1,5 @@
+// by duckate, Kate Sychenko
+
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <stdio.h>
@@ -8,21 +10,7 @@
 
 #define SIZE 1024
 #define SERVER_PORT 8080
-#define BACKLOG 10 // how many pending connections queue will hol
-
-
-// void send_file(FILE *fp, int sockfd){
-//   int n;
-//   char data[SIZE] = {0};
- 
-//   while(fgets(data, SIZE, fp) != NULL) {
-//     if (send(sockfd, data, sizeof(data), 0) == -1) {
-//       perror("[-]Error in sending file.");
-//       exit(EXIT_FAILURE);
-//     }
-//     bzero(data, SIZE);
-//   }
-// }
+#define BACKLOG 10 // how many pending connections queue will hold
 
 int main(int argc, char const* argv[]) {  
 
@@ -30,24 +18,12 @@ int main(int argc, char const* argv[]) {
     int server_fd, new_socket, valread; 
     char buffer[1024] = { 0 };
     char* hello = "Hello from server\n";
-    // FILE *fp;
-    // fp = fopen ("index.html", "w+");
-
-    // int i;
-    // // char fn[150];
-    // char str[] = "<!DOCTYPE html><html><body><h1>Привет медвед</h1></body></html>\n";
-    //     for (i = 0; str[i] != '\n'; i++) {
-    //         /* write to file using fputc() function */
-    //         fputc(str[i], fp);
-    //     }
-    // fclose (fp); 
-
     int addrlen = sizeof(address);
     
-// create a socket
-// -1 if error
+    // create a socket
+    // -1 if error
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("SERVER: socket creation failed");
+        perror("[-] SERVER: socket creation failed");
         exit(EXIT_FAILURE);
     }
 
@@ -58,69 +34,60 @@ int main(int argc, char const* argv[]) {
 
     // get rid of "Address already in use" error
     if ((setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes))) < 0) {
-        perror("[-]setsockopt failed");
+        perror("[-] SERVER: setsockopt failed");
         exit(EXIT_FAILURE);
     }
 
     if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
-        perror("SERVER: bind failed");
+        perror("[-] SERVER: bind failed");
         exit(EXIT_FAILURE);
     }
 
     if (listen(server_fd, BACKLOG) < 0) {
-        perror("SERVER: listen failed");
+        perror("[-] SERVER:: listen failed");
         exit(EXIT_FAILURE);
     }
 
-while (1) {    
-    if ((new_socket = accept(
-                            server_fd, 
-                            (struct sockaddr*)&address, 
-                            (socklen_t*)&addrlen
-                            )
-    ) < 0) {
-        perror("SERVER: accept failed");
-        exit(EXIT_FAILURE);
-    }
+    while (1) {    
+        if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
+            perror("[-] SERVER: accept failed");
+            exit(EXIT_FAILURE);
+        }
 
-    // char request[] = "HTTP/1.1 200 OK\r\n\r\n<html>\n\r<body>\n\r\rhello\n\r</body>\n</html>";
+    read(new_socket, buffer, SIZE);
+    printf("[+] CLIENT SAYS: %s", buffer);
+
     char response[] = "HTTP/1.1 200 OK\r\n\n\r<!DOCTYPE html><html>\n\r<body>\n\r<h1>Kate is here</h1><p>Typing from the server.</p></body></html>";
     send(new_socket, response, strlen(response), 0);
+    printf("[+] SERVER: Package is sent\n");
 
-    // recv(new_socket, buffer, strlen(buffer), 0);
-    //     printf("%s", buffer);
+    // find out client's address and port
+    socklen_t len;
+    struct sockaddr_storage addr;
+    char ipstr[INET6_ADDRSTRLEN];
+    int port;
+    len = sizeof addr;
 
-    // recv(new_socket, buffer, strlen(buffer), 0);
-    //     printf("%s", buffer);
+    getpeername(new_socket, (struct sockaddr*)&addr, &len);
+     // deal with both IPv4 and IPv6:
+    if (addr.ss_family == AF_INET) {
+        struct sockaddr_in *s = (struct sockaddr_in *)&addr;
+        port = ntohs(s->sin_port);
+        inet_ntop(AF_INET, &s->sin_addr, ipstr, sizeof ipstr);
+    } else { // AF_INET6
+        struct sockaddr_in6 *s = (struct sockaddr_in6 *)&addr;
+        port = ntohs(s->sin6_port);
+        inet_ntop(AF_INET6, &s->sin6_addr, ipstr, sizeof ipstr);
+    }
 
-    // send(new_socket, request, strlen(request), 0);
+    printf("[+] Peer IP address: %s\n", ipstr);
+    printf("[+] Peer port : %d\n", port);
 
-
-
-//TODO: РАЗОБРАТЬ 
-    // valread = read(new_socket, buffer, 1024);
-    // printf("SERVER BUFFER: %s\n", buffer);
-    // send(new_socket, fp, strlen(fp), 0);
-    // send(new_socket, hello, strlen(hello), 0);
-    // send_file(fp, new_socket);
-    printf("SERVER: Hello message sent\n");
-
-    // getpeername(new_socket, (struct sockaddr *)&test, (socklen_t*)sizeof(test));
-    
-    // char remotename[50];
-    // inet_ntop(AF_INET, &test, remotename, sizeof(remotename));
-    // printf("%s\n", remotename);
-    // printf("address: %d, port: %d\n", test.sin_addr.s_addr, test.sin_port);
-    // printf("server_fd: %d\n", server_fd);
-    // printf("new_socket: %d\n", new_socket);
-
-
-    // read(STDIN_FILENO, buffer, 10);
-
-    // closing the connectesocket
+    // closing the connect socket
     close(new_socket);
     }
     // closing the listening socket
+    close(server_fd);
 
     // int status;
     // status = shutdown(server_fd, SHUT_RDWR);
